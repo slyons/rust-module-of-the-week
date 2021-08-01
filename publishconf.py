2493208839759a8377727e57d0f25a4d8079b540
Scott Lyons <scottalyons@gmail.com>
2021-08-01 15:28:01 -0700

    Typo

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = ''
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}}.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
